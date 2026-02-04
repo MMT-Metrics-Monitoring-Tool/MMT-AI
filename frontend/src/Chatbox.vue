943150db81803042dca456190d9f830295eab416
Esa Karjalainen <77407605+sdeska@users.noreply.github.com>
2026-02-04 17:00:13 +0200

    Fix front-end target ports

--- frontend/src/Chatbox.vue
+++ frontend/src/Chatbox.vue
@@ -32,7 +32,7 @@ const projectId = inject("projectId");
 const messagesContainer = useTemplateRef('messagesContainer');
 
 const startSession = async () => {
-  const res = await fetch("http://localhost:5000/start_session", {
+  const res = await fetch("http://localhost:8000/start_session", {
     method: "GET",
     headers: token.value ? { Authorization: token.value } : {}
   });
@@ -58,7 +58,7 @@ const sendMessage = async () => {
   loading.value = true;
   
   try {
-    const res = await fetch( "http://localhost:5000/chat", {
+    const res = await fetch( "http://localhost:8000/chat", {
       method: "POST",
       headers: {
         "Authorization": token.value,
